447b7bf5153301aa8fd8bd152a707fa6aa26a077
SophiaHughes <skh372@nyu.edu>
2026-05-08 00:57:00 -0400

    updated zoom

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ window.onload = () => {
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v11',
     center: [-118.35, 34.15],
-    zoom: 10.8,
+    zoom: 9.0,
     interactive: false
   });
 
