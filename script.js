f6b9d72d32b8879964b0c982a31255bec284fa65
SophiaHughes <skh372@nyu.edu>
2026-05-10 03:34:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -267,9 +267,9 @@ window.onload = () => {
   const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/light-v11',
-      center: [-118.42, 34.05],
+      center: [-118.40, 34.05],
       zoom: 9.0,
-      minZoom: 8.8,      // ← can't zoom out past this
+      minZoom: 8.5,      // ← can't zoom out past this
       maxZoom: 13,       // ← can't zoom in past this
       interactive: true  // ← re-enable interaction so zoom works
   });
